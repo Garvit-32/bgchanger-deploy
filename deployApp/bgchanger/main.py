a5bc02562fe2e105e319a4c79b692de631b33623
Garvit-32 <garvitgarg32@gmail.com>
2021-05-29 18:24:01 +0530

    fix weight path

--- deployApp/bgchanger/main.py
+++ deployApp/bgchanger/main.py
@@ -34,7 +34,7 @@ def process(foreground, background):
     #model is created
     model = hrnet(2)
     model.load_state_dict(
-        torch.load('./deployApp/bgchanger/hrnetv2_hrnet18_person_dataset_120.pth', map_location=torch.device("cpu"))[
+        torch.load('deployApp/bgchanger/hrnetv2_hrnet18_person_dataset_120.pth', map_location=torch.device("cpu"))[
             "state_dict"]
     )  # model is loaded
     model.eval()
